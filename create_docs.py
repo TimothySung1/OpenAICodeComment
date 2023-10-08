da48af55fbb1f42b7239cb33acf200303de6dea1
Timothy Sung <tsung8@gatech.edu>
2023-10-07 21:28:09 -0400

    added sphinx config and basic build functionality

--- create_docs.py
+++ create_docs.py
@@ -1,14 +1,23 @@
 from sphinx.ext.apidoc import main
-import sphinx.builders
-import sphinx.application
+import sphinx.application as sphinx
+import conf as conf
 
 # create sphinx documentation for the project
 def create_sphinx_docs(project_path):
-    main(['-o', './docs', '.'])
+    # create rst files for each python class
+    main(['-o', './source', project_path])
+    # create index.rst
+    
 
 def sphinx_build():
     # sphinx.builders.Builder.build(['main.rst'], "summary")
-    pass
+    conf.project = input("Enter the project name: ")
+    conf.copyright = input("Enter copyright/license: ")
+    conf.copyright = input("Enter author(s): ")
 
-create_sphinx_docs('')
-sphinx_build()
+    app = sphinx.Sphinx(srcdir="./source", confdir=".", outdir="./build/html", doctreedir="./build/doctrees", buildername="html")
+    app.build()
+
+if __name__ == "__main__":
+    create_sphinx_docs('.')
+    sphinx_build()
